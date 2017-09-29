86260092f3cd675279adb6fc6c5d7284ebca90a5
wilfrank84 <wilfrank84@gmail.com>
2017-09-28 23:51:05 -0300

    Adding Tasks Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './navbar/navbar.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
